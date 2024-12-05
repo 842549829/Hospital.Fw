b852c77c8428b0f60e10d74cc6357546d1dc52e4
rb <rb@DESKTOP-HB00T2C>
2024-12-05 13:10:33 +0800

    feat: add EntityDto

---
+++ src/Hospital.Fw.Application.Contract/EntityDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Hospital.Fw.Application.Contract;
+
+public class EntityDto<T> : EntityDto, IEntityDto<T>
+{
+    public T Id { get; set; } = default!;
+}
+
+public class EntityDto : IEntityDto;
+
+public interface IEntityDto;
+
+public interface IEntityDto<T>
+{
+    public T Id { get; set; }
+}
