5c7ccd2da19d1dca6899c8d8f88388af77b0ddd7
rb <842549829@qq.com>
2025-11-03 10:59:37 +0800

    feat(openapi): 添加OpenAPI到TypeScript客户端代码生成器- 新增 OpenApiToTsClient 模块用于将OpenAPI文档转换为TypeScript客户端代码- 在测试项目中引入对新模块的引用并注册相关服务- 添加全局JSON配置以指定SDK版本及运行时策略 - 创建控制器接口实现从Swagger端点获取并生成TypeScript文件的功能 - 支持通过URL异步转换OpenAPI定义并保存为本地TS文件- 提供路径和内容记录类以便于管理生成的客户端代码文件

---
+++ test/Hospital.Fw.Test/Controllers/OpenapiController.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Hospital.Fw.OpenApiToTsClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Fw.Test.Controllers;
+
+[ApiController]
+[Route("/api/open")]
+public class OpenapiController : ControllerBase
+{
+    [HttpGet]
+    public async Task<List<PathAndContent>> Test()
+    {
+        var openApiToTypeScriptConverter = new OpenApiToTypeScriptConverter();
+        var tsClientCode =
+            await openApiToTypeScriptConverter.ConvertFromUrlAsync("http://localhost:5268/swagger/v1/swagger.json");
+        var tsClientCodes = tsClientCode.Select(x => new PathAndContent(x.Path, x.Content)).ToList();
+        
+        var path = Path.Combine(AppContext.BaseDirectory, "ts");
+        foreach (var item in tsClientCodes)
+        {
+            var filePath = Path.Combine(path, item.Path);
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath) ?? string.Empty);
+            await System.IO.File.WriteAllTextAsync(filePath, item.Content, Encoding.UTF8);
+        }
+        
+        
+        return tsClientCodes;
+    }
+}
+
+public record PathAndContent(string Path, string Content);  
