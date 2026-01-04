ae6c183a8c3f4fbd6ffa806a21c369fe5f15dc64
rb <842549829@qq.com>
2026-01-04 09:52:23 +0800

    feat:新增租户 组织Id

---
+++ src/Hospital.Fw.Domain/Entities/IHasOrganizationId.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital.Fw.Domain.Entities;
+
+/// <summary>
+/// 组织Id
+/// </summary>
+public interface IHasOrganizationId
+{
+    /// <summary>
+    /// 组织Id
+    /// </summary>
+    public string OrganizationId { get; set; }
+}
---
+++ src/Hospital.Fw.Domain/Entities/IHasTenantId.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital.Fw.Domain.Entities;
+
+/// <summary>
+/// 租户Id
+/// </summary>
+internal interface IHasTenantId
+{
+    /// <summary>
+    /// 组织Id
+    /// </summary>
+    public string OrganizationId { get; set; }
+}
