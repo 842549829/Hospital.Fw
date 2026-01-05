9adb438e8ec6c8a6675eba1acb2d543ff5951e3f
rb <842549829@qq.com>
2026-01-05 09:05:03 +0800

    feat: 租户管理

---
+++ src/Hospital.Fw.Domain/Entities/IMultiTenantEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Hospital.Fw.Domain.Entities;
+
+/// <summary>
+/// 多租户
+/// </summary>
+public interface IMultiTenantEntity : IHasTenantId, IHasOrganizationId, IHasOrganizationCode;
---
+++ src/Hospital.Fw.Domain/Entities/MultiTenantFullAuditedEntity.cs
@@ -0,0 +1,28 @@
+﻿using Hospital.Fw.Domain.Shared.Constant;
+using SqlSugar;
+
+namespace Hospital.Fw.Domain.Entities;
+
+/// <summary>
+/// 多租户审计类
+/// </summary>
+public abstract class MultiTenantFullAuditedEntity : FullAuditedEntity<string>, IMultiTenantEntity
+{
+    /// <summary>
+    /// 租户Id
+    /// </summary>
+    [SugarColumn(Length = SqlSugarCoreDbConst.SugarColumnLength32, IsNullable = false, ColumnDescription = "租户Id")]
+    public string TenantId { get; set; } = null!;
+
+    /// <summary>
+    /// 组织Id
+    /// </summary>
+    [SugarColumn(Length = SqlSugarCoreDbConst.SugarColumnLength32, IsNullable = false, ColumnDescription = "组织Id")]
+    public string OrganizationId { get; set; } = null!;
+
+    /// <summary>
+    /// 组织code
+    /// </summary>
+    [SugarColumn(Length = 514, IsNullable = false, ColumnDescription = "组织code")]
+    public string OrganizationCode { get; set; } = null!;
+}
