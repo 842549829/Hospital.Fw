1bf23661fb3931db431572d0cc58323a34e26f13
rb <842549829@qq.com>
2026-01-05 08:56:36 +0800

    feat:添加组织Code

---
+++ src/Hospital.Fw.Domain/Entities/IHasOrganizationCode.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital.Fw.Domain.Entities;
+
+/// <summary>
+/// 组织Code
+/// </summary>
+public interface IHasOrganizationCode
+{
+    /// <summary>
+    /// 组织code
+    /// </summary>
+    string OrganizationCode { get; set; }
+}
