3f1871da30c29f129763b656f4f6be76e7f2e23a
rb <842549829@qq.com>
2026-01-06 09:20:50 +0800

    fix:修改实体

--- src/Hospital.Fw.Application.Contract/Dto/CreateFullNameDto.cs
+++ src/Hospital.Fw.Application.Contract/Dto/CreateFullNameDto.cs
@@ -5,7 +5,7 @@ namespace Hospital.Fw.Application.Contract.Dto;
 /// <summary>
 /// 带名称的审计实体-创建
 /// </summary>
-public abstract class CreateFullNameDto : EntityDto<string>, IHasNameDto
+public abstract class CreateFullNameDto : EntityDto, IHasNameDto
 {
     /// <summary>
     /// 名称
